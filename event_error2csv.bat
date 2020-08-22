bbb3f2f11cea6b550e996c65cfe7a57cb0a0fbf6
KentaGoto <gotokentag@gmail.com>
2020-08-22 14:02:49 +0900

    Update event_error2csv.bat

--- event_error2csv.bat
+++ event_error2csv.bat
@@ -2,13 +2,13 @@
 
 setlocal
 
-rem システムのイベントエラーを csv に書き出す
+rem Exporting system event errors to csv
 wmic ntevent where "(logfile='system' and timegenerated >= '%DATE:/=%' and (EventType='1' or EventType='2'))" list /format:CSV > "%~dp0%COMPUTERNAME%_%DATE:/=%_sys.csv"
 
-rem アプリケーションのイベントエラーを csv に書き出す
+rem Exporting application event errors to csv
 wmic ntevent where "(logfile='application' and timegenerated >= '%DATE:/=%' and (EventType='1' or EventType='2'))" list /format:CSV > "%~dp0%COMPUTERNAME%_%DATE:/=%_app.csv"
 
 echo Complete!
-echo それぞれのログは %~dp0% に出力されています。
+echo Each log is written to %~dp0%
 
 endlocal
